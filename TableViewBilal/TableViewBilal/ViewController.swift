95a597e4c4e59c09ec2e6f994bbf0eff39bea978
Bilal Gökpinar / BBS2H20A <Bilal.Goekpinar@edu.bib.de>
2021-11-09 08:31:15 +0100

    Projekt angelegt

---
+++ TableViewBilal/TableViewBilal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewBilal
+//
+//  Created by Bilal Gökpinar / BBS2H20A on 09.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
