4476e7836d6c0667e627e436dc4736a51f83e72d
Bilal Gökpinar / BBS2H20A <Bilal.Goekpinar@edu.bib.de>
2021-11-15 09:21:17 +0100

    Projekt fertig gestellt

--- TableViewBilal/TableViewBilal/ViewController.swift
+++ TableViewBilal/TableViewBilal/ViewController.swift
@@ -8,90 +8,132 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    // Unsere DataSource
-    var games = ["Far Cry 6", "Battlefield 2042", "GTA"]
-    
-    
-    @IBOutlet weak var MyTableView: UITableView!
-    
-    
 
+    //Unser Datasource
+    var games = ["Far Cry 6", "Battlefield 2042", "God of War", "Rocket League"]
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        MyTableView.dataSource = self
-        MyTableView.delegate = self
+        myTableView.dataSource = self
+        myTableView.delegate = self
         
-        // Hinzufügen dess Edit-Button
+        //Hinzufügen des Edit-Buttons
         navigationItem.rightBarButtonItem = editButtonItem
         
-        // Title
+        //Titel für die View
         title = "Games"
         
-        //leere UIView um die Tableview zu "kürzen"
-        MyTableView.tableFooterView = UIView()
-        
+        //leeres UIView um die TableView zu "kürzen"
+        myTableView.tableFooterView = UIView()
     }
     
     override func setEditing(_ editing: Bool, animated: Bool) {
-        print ("Set Editing")
+        print("Set Editing")
         
-        // Wenn Editing False ist, wird es auf True gesetzet und auch umgekehrt
+        //Wenn Editing False ist, wird es auf True gesetzt und umgekehrt
         super.setEditing(!isEditing, animated: true)
-        MyTableView.setEditing(!MyTableView.isEditing, animated: true)
+        myTableView.setEditing(!myTableView.isEditing, animated: true)
     }
-
-
+    
+    //Funktion des Add-Buttons
+    @IBAction func addButtonFunction(_ sender: UIBarButtonItem) {
+        
+        //Anlegend des Textfields für die Benutzung im Alert
+        var textField = UITextField()
+        
+        //Anlegen des Alerts
+        let alert = UIAlertController(title: "Neues Game hinzufügen", message: "Bitte gib das Game ein", preferredStyle: .alert)
+        
+        //Funktion für den Alert
+        let action = UIAlertAction(title: "Hinzufügen", style: .default, handler: { (action) in
+            //print("Hinzufügen gedrückt")
+            if textField.text == "" || textField.text == nil {
+                return
+            } else {
+                //Einfügen der Eingabe in den Array
+                self.games.insert(textField.text!, at: 0)
+                
+                //Anlegen eines eigenen Index-Path, da diese Funktion diesen nicht mitgibt
+                let indexPath = IndexPath(row: 0, section: 0)
+                
+                //Neuer Wert soll in die TableView-Cell eingefügt werden:
+                self.myTableView.insertRows(at: [indexPath], with: .automatic)
+            }
+        })
+        
+        //Dem Alert die Action zuweisen
+        alert.addAction(action)
+        
+        //Dem Alert das TextField zuweisen
+        alert.addTextField(configurationHandler: { (alertTextField) in
+            //Placeholder fürs TextField
+            alertTextField.placeholder = "Game"
+            
+            //Zuweisung der Eingabe (neuer Wert) in die TableView
+            textField = alertTextField
+        })
+        
+        //Den Alert anzeigen (presenten)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
-// Erweiterung unserer Klasse ViewController
-extension ViewController: UITableViewDataSource{
+//---------------------------------------------------------------------------------------------------------------
+
+//Erweiterung unserer Klasse ViewController
+extension ViewController: UITableViewDataSource {
     
-    // Wieviele Zellen(Cells) sollen erstellt werden
+    //Wieviele Zellen (Cells) sollen erstellt werden
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return games.count
     }
     
-    // Wie sollen die Cells aussehen
+    //Wie sollen die Cells aussehen
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let myCell = MyTableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
-            myCell.textLabel?.text = games[indexPath.row]
-            return myCell
+        let myCell = myTableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        
+        myCell.textLabel?.text = games[indexPath.row]
+        
+        return myCell
     }
 }
 
+//---------------------------------------------------------------------------------------------------------------
 
-// Erweiterung unserer Klasse ViewController
-extension ViewController: UITableViewDelegate{
+//Erweiterung unserer Klasse ViewController
+extension ViewController: UITableViewDelegate {
     
+    //Funktion zum "Bewegen" der Cell
     func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        return true;
+        return true
     }
     
-    // Funktion zum Verschieben der Cell
+    //Funktion zum "Verschieben" der Cell
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         
-    
-    // Konstante für die ausgewählte Cell
+        //Konstante für die ausgewählte Cell
         let selectedGame = games[sourceIndexPath.row]
         
-    // Funktion zum Löschen der Cell im Array
+        //Funktion zum Löschen der Cell im Array
         games.remove(at: sourceIndexPath.row)
         
-    // Funktion zum Einfügen des neuen Wertes im Array
+        //Funktion zum Einfügen des neuen Wertes im Array
         games.insert(selectedGame, at: destinationIndexPath.row)
+    }
+    
+    //Funktion zum Löschen der Cells
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         
-    // Funktion zum Löschen der Cells
-        func tableView( tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath){
+        if editingStyle == .delete {
+            //Löschen aus dem Array
+            games.remove(at: indexPath.row)
             
-            if editingStyle == .delete {
-                // Löschen aus dem Array
-                games.remove(at: indexPath.row)
-                
-                // Löschen aus der TableView
-                MyTableView.deleteRows(at: [indexPath], with: .automatic)
-            }
+            //Löschen aus der TableView
+            myTableView.deleteRows(at: [indexPath], with: .automatic)
         }
     }
 }
